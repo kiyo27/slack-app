75f81c203b47db7fb9b02c9a2382caf10091ac9c
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2019-10-03 21:43:24 +0900

    modify

--- upload-file/date-lib.js
+++ upload-file/date-lib.js
@@ -4,7 +4,7 @@ exports.yearAndMonthAndDay = function() {
     const month = today.getMonth() + 1;
     const day = today.getDate();
 
-    let monthAndDay = ((month < 10) ? '0' + month.toString() : month);
-    monthAndDay += ((day < 10) ? '0' + day.toString() : day);
+    let monthAndDay = ((month < 10) ? '0' + month.toString() : month.toString());
+    monthAndDay += ((day < 10) ? '0' + day.toString() : day.toString());
     return `${year}${monthAndDay}`;
 }
